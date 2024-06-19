24e5e7e0f753990db1e32175fb2087c120dacf21
NarongDan <narong.dan1@gmail.com>
2024-06-19 14:58:35 +0700

    verify

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const PORT = 8000
+const PORT = 8000;
 
+app.use("/verify", (req, res) => {
+  res.status(200).json({ msg: "verified already" });
+});
 
-
-app.listen(PORT, () => { console.log(`SERVER ON PORT ${PORT}`); })
+app.listen(PORT, () => {
+  console.log(`SERVER ON PORT ${PORT}`);
+});
