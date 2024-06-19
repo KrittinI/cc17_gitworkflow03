44be3f7fe6692de3b2cf263787717e2a62cbc5a4
korkiat0 <korkiat_bang@hotmail.co.th>
2024-06-19 14:58:53 +0700

    login ok

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const PORT = 8000
+const PORT = 8000;
 
+app.use("/login", (req, res) => {
+  res.json({ msg: "Login......" });
+});
 
-
-app.listen(PORT, () => { console.log(`SERVER ON PORT ${PORT}`); })
+app.listen(PORT, () => {
+  console.log(`SERVER ON PORT ${PORT}`);
+});
