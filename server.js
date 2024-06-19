db58a7d96f649c983c8b042c904a02170232f1dd
NarongDan <narong.dan1@gmail.com>
2024-06-19 15:07:58 +0700

    add sendFile

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use("/verify", (req, res) => {
   res.status(200).json({ msg: "verified already" });
 });
 
+app.use("/sendFile", (req, res) => {
+  res.json({ msg: "file received" });
+});
+
 app.listen(PORT, () => {
   console.log(`SERVER ON PORT ${PORT}`);
 });
